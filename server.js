915f3a39852e0a51bd42cd4e4811356126f78b21
Oleh Hatsenko <40500897+IRONKAGE@users.noreply.github.com>
2020-01-22 12:56:58 +0200

    add mongodb (#1)
    * add mongodb
    
    * little reverso
    
    * знову все переробив
    
    * Добалений парсер, нормально опрацьована робота з клієнтами, сервер оптимізовано
    
    * all done
    
    * видалив закочімені коди - тепер код чистий
    
    * Merge
    
    * доведений до ума сервер, але Swagger так і не згенерувався (помилка Fetch error)
    
    * modified:   .gitignore
    modified:   app.js
    modified:   package-lock.json
    
    * del folder node_modules
    
    * допрацювання над HTTP-командами та налаштування Swagger вже для Crud.Users + приведення в порядок код
    
    * деякі вдосконалення коду
    
    * перекочувло з new story
    
    Co-authored-by: Lolibai <mikleshef@gmail.com>

--- server.js
+++ server.js
@@ -1,47 +1,6 @@
-var express = require('express');
-var app = express();
-var crud_users = [];
-const port = 3000;
-const host = "localhost";
+var http = require('http');
+var app = require('./app');
+var port = process.env.PORT || 15000;
+var server = http.createServer(app);
 
-app.get('/crud-users', function (request, response){
-    var crudUserName = request.param('crudUserName');
-    if (crudUserName){
-        crud_users.forEach((item) => {
-            if (item.crudUserName === crudUserName){
-                response.send(item);
-                return;
-            };
-        });
-    };
-    response.send(crud_users);
-});
-
-app.post('/crud-users', function (request, response){
-    var crudUserName = request.param('crudUserName');
-    if (crudUserName){
-        crud_users.push({
-            crudUserName,
-        });
-    }else{
-        response.res.status(500).json('о ні');
-    }
-    response.send(crud_users);
-});
-
-app.delete('/crud-users', function (request, response){
-    var crudUserName = request.param('crudUserName');
-    if (crudUserName){
-        crud_users.forEach((item) => {
-            if (item.crudUserName === crudUserName){
-                response.send(item);
-                return;
-            };
-        });
-    };
-    response.send(crud_users);
-});
-
-app.listen(port, host, function(){
-    console.log('Сервер запустився на хості: localhost, port: 3000');
-})
+server.listen(port);
