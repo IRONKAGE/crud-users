ff303a57a010cc9013aec2018e8fdc682e7aa28e
Oleh Hatsenko <40500897+IRONKAGE@users.noreply.github.com>
2020-01-23 16:40:19 +0200

    Delete mongo.query.js

--- api/db/mongo.query.js
+++
@@ -1,7 +0,0 @@
-var CrudSkills = require('../models/crud.skills.models');
-
-exports.min_age = ((request, response, next) => {
-    response.status(200).json({
-        message: "Навички користувачів витягнено"
-    });
-});
